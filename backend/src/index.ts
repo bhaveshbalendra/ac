6e791697323c7b0ec6cf4b01bbe43240799aece0
bhaveshbalendra <balendrabhavesh@gmail.com>
2024-08-05 21:47:17 +0530

    added lists from backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from "express";
 import { sampleProduct } from "./data";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }));
 const PORT = 4000;
 
 app.get("/api/products", (req: Request, res: Response) => {
