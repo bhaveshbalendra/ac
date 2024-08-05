0190bc0fda5e372c56e507c42101187903a40a3f
bhaveshbalendra <balendrabhavesh@gmail.com>
2024-08-05 15:37:41 +0530

    added list products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
 import "./App.css";
+import { sampleProduct } from "./data";
 
 function App() {
   return (
     <div>
       <header>Amazon</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProduct.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>Rs {product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   );
